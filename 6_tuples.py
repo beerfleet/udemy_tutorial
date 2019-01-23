698573bbc7bdb2a6404d71b72a7a8e182a4551a9
Jan Van Biervliet <jvbiervliet@gmail.com>
2019-01-23 15:17:38 +0100

    tuples bug

--- 6_tuples.py
+++ 6_tuples.py
@@ -3,13 +3,14 @@ Created on 23 jan. 2019
 
 @author: jan
 '''
-tuple1 = ('Abbie', 'Brynn', 'Corinne')
+tuple1 = ('Abbie', 'Brynn', 'Corinne', 'Deodoro')
 tuple_to_list = list(tuple1)
 
 print(tuple_to_list)
 print(type(tuple_to_list))
 
-tuple_to_list.remove('Abbie')
+del tuple_to_list[0]
 new_tuple = tuple(tuple_to_list)
+
 print(new_tuple)
 print(type(new_tuple))
