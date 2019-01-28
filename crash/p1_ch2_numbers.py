5c676360582fadb6b382fee0b770233c0b47c549
jvanbiervliet <jan.van.biervliet@etion.be>
2019-01-28 16:23:07 +0100

    ch2, ch3

---
+++ crash/p1_ch2_numbers.py
@@ -0,0 +1,35 @@
+'''
+Created on 28 jan. 2019
+
+@author: jvanbiervliet
+'''
+
+# 2-8
+def acht_plus():
+    print (5 + 3)
+
+def acht_min():
+    print (99 - 91)
+
+def acht_maal():
+    print (2 * 4)
+
+def acht_gedeeld():
+    print ( 800 / 100)
+
+# 2-9
+def favourite_number():    
+    nummer = 666
+    print('Mijn favoriete nummer is: ' + str(nummer) + " woot woot !!!")
+
+def voer_uit():
+    print('2-8')
+    acht_plus()
+    acht_min()
+    acht_maal()
+    acht_gedeeld()    
+    print('2-9')
+    favourite_number()
+    
+voer_uit()
+
---
+++ crash/p1_ch3_lists_1.py
@@ -0,0 +1,5 @@
+'''
+Created on 28 jan. 2019
+
+@author: jvanbiervliet
+'''
