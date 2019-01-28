65d014b300d3bfd72848b5beb316cb1747184f67
jvanbiervliet <jan.van.biervliet@etion.be>
2019-01-28 16:29:35 +0100

    ch3 ex

--- crash/p1_ch3_lists_1.py
+++ crash/p1_ch3_lists_1.py
@@ -3,3 +3,21 @@ Created on 28 jan. 2019
 
 @author: jvanbiervliet
 '''
+lijst = []
+
+def bewaar_namen():
+    return ['Jakke', 'Pjer', 'Inventao']
+
+def druk_een_voor_een():
+    print( lijst[0] )
+    print( lijst[1] )
+    print( lijst[2] )
+    
+def druk_met_boodschap():
+    print ('Dit is een test ' + lijst[0])
+    print ('Dit is een test ' + lijst[1])
+    print ('Dit is een test ' + lijst[2])
+
+lijst = bewaar_namen()
+druk_een_voor_een()
+druk_met_boodschap()
