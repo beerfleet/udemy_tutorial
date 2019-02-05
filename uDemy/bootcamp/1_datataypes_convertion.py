29ef810c58d30ad63dacdea75a4a6e1044815a38
jvanbiervliet <jan.van.biervliet@etion.be>
2019-02-05 15:48:50 +0100

    mijl naar kilometer

--- uDemy/bootcamp/1_datataypes_convertion.py
+++ uDemy/bootcamp/1_datataypes_convertion.py
@@ -3,3 +3,8 @@ Created on 5 feb. 2019
 
 @author: jvanbiervliet
 '''
+
+kilometers = input("Geef het aantal kilometers: ")
+mijl = round(float(kilometers) / 1.609344, 2)
+print("{} kilometers is {} mijl".format(kilometers, mijl))
+
