a4fb7f2f321dc9d1fc09bce9a66976fff17ca7c2
jvanbiervliet <jan.van.biervliet@etion.be>
2019-02-13 14:45:44 +0100

    oefening steen schaar papier

---
+++ uDemy/bootcamp/4_rock_paper_scissors.py
@@ -0,0 +1,43 @@
+'''
+Created on 6 feb. 2019
+
+@author: jvanbiervliet
+'''
+
+player1 = ""
+player2 = ""
+
+player1 = input("Player1, choose (r)ock, (p)aper, (s)cissors: ")
+for i in range(0, 100):
+    print(" * * * * * * * * * * *")    
+player2 = input("Player2, choose (r)ock, (p)aper, (s)cissors: ")
+
+win_text = ""
+
+if player1 == 'r':
+    win_text = "Player 1 chose rock.. "
+    if player2 == 'p':
+        win_text += "Player 2 wins !!!"
+    elif player2 == 'r':
+        win_text = "it\'s a tie"
+    else:
+        win_text += "Player 1 wins !!!"
+elif player1 == 'p':
+    win_text = "Player 1 chose paper.. "
+    if player2 == 'r':
+        win_text += "Player1 wins !!!"
+    elif player2 == 's':
+        win_text += "Player 2 wins !!!"
+    else:
+        win_text = "It\'s a tie"
+elif player1 == 's':
+    win_text = "Player 1 chose scissors.. "
+    if player2 == 'r':
+        win_text += "Player 2 wins !!!"
+    elif player2 == 'p':
+        win_text += "Player1 wins !!!"
+    else:
+        win_text = "It\'s a tie"
+        
+print(win_text)
+    
