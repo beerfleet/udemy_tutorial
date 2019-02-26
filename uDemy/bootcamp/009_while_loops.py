4bcbad77777403288fc46cab2070f89e3e603169
jvanbiervliet <jan.van.biervliet@etion.be>
2019-02-26 13:42:32 +0100

    extra loop oefeningen

--- uDemy/bootcamp/009_while_loops.py
+++ uDemy/bootcamp/009_while_loops.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def geheim_paswoord():
     paswoord = 'Inagat'
     invoer = input("Wat is het geheime wachtwoord ? : ")
@@ -23,5 +25,32 @@ def stop_copying():
         zin = input(" -> ")
     print("OK JIJ WINT")
     
+
+def voer_uit_tot_i_is_vijf():
+    i = 0
+    nummer = 0
+    while nummer != 5:
+        nummer = randint(1,  10)
+        i += 1
+    print(f"Het duurde {i} keer om 5 te genereren ... ")
+
+
+def raad_getal():
+    opnieuw = 'j'
+    while opnieuw.upper() == "JA" or opnieuw.upper() == "J" :
+        geheim_getal = randint(1,  10)
+        raad_getal = 0
+        uitvoer = ""
+        while raad_getal != geheim_getal:
+            raad_getal = int(input("Raad een getal tussen 1 en 10: "))
+            if raad_getal < geheim_getal:
+                print("Geraden getal is te klein ... probeer opnieuw")
+            elif raad_getal > geheim_getal:
+                print("Geraden getal is te groot ... probeer opnieuw")
+            else:
+                break;
+        print("Het geheim getal is inderdaad {}".format(geheim_getal))
+        opnieuw = input("Wil je opnieuw spelen ? : ")
     
-stop_copying()
+
+raad_getal()
