35f7cb5297e6d8c119add95a0a61e5c415a1fb70
jvanbiervliet <jan.van.biervliet@etion.be>
2019-02-26 10:25:25 +0100

    combined exercise finished

--- uDemy/bootcamp/008_combined_exercise.py
+++ uDemy/bootcamp/008_combined_exercise.py
@@ -1,1 +1,11 @@
-
+def oefening():
+    uitvoer = ""
+    for nummer in range(1,  21):
+        if nummer == 4 or nummer == 13:
+            uitvoer = f"{nummer} is ONGELUKSGETAL"
+        elif nummer % 2 == 0:
+            uitvoer = f"{nummer} is even"
+        else:
+            uitvoer = f"{nummer} is oneven"
+        print(uitvoer)
+oefening()
