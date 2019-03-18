cf9b99e09f491847d2a19d43a26e13e05a8a3b6a
jvanbiervliet <jan.van.biervliet@etion.be>
2019-03-18 15:12:19 +0100

    one more function exercide

--- uDemy/bootcamp/018_function_exercises_2.py
+++ uDemy/bootcamp/018_function_exercises_2.py
@@ -35,7 +35,7 @@ def some_kwargs(kwarg_1, kwarg_2, kwarg_3):
     print("kwarg_3:", kwarg_3)
 
 kwargs = {"kwarg_1": "Val", "kwarg_2": "Harper", "kwarg_3": "Remy"}
-#some_kwargs(**kwargs)
+# some_kwargs(**kwargs)
 
 def greet_me(**kwargs):
     if kwargs is not None:
@@ -57,3 +57,45 @@ def combine_words(word,  **args):
 # print(combine_words("Tette",  suffix="zot",  prefix="mega"))
 # print(combine_words("Tette",  prefix="kwar"))
 
+def sommation(*args):
+    sum = 0
+    for nr in args:
+        sum += nr
+    return sum
+
+numbers = [1,  66,  99,  88]
+
+# print(sommation(*numbers))
+
+# NO TOUCHING! =================================================================
+def count_sevens(*args):
+    return args.count(7)
+
+nums = [90,1,35,67,89,20,3,1,2,3,4,5,6,9,34,46,57,68,79,12,23,34,55,1,90,54,34,76,8,23,34,45,56,67,78,12,23,34,45,56,67,768,23,4,5,6,7,8,9,12,34,14,15,16,17,11,7,11,8,4,6,2,5,8,7,10,12,13,14,15,7,8,7,7,345,23,34,45,56,67,1,7,3,6,7,2,3,4,5,6,7,8,9,8,7,6,5,4,2,1,2,3,4,5,6,7,8,9,0,9,8,7,8,7,6,5,4,3,2,1,7]
+# NO TOUCHING! =================================================================
+
+# Write your code below:
+
+result1 = count_sevens(*[1,  4,  7])
+result2 = count_sevens(*nums)
+
+# print (f"r1:{result1},  r2:{result2}")
+
+def calculate(make_float, operation, first,  second, message="The result is"):
+    result = 0
+    if operation == 'add':
+        result = first + second
+    elif operation == 'subtract':
+        result = first - second
+    elif operation == 'multiply':
+        result = first * second
+    elif operation == 'divide':
+        result = first / second
+    if make_float:
+        result = float(result)
+    else:
+        result = int(result)
+    return "{} {}".format(message,  result)
+
+# print(calculate(make_float=False, operation='add', message='You just added', first=2, second=4))
+# print(calculate(make_float=True, operation='divide', first=3.5, second=5))
