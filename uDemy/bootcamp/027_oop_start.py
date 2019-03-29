de7b8d1581611480645764a4e6cfb6718b1cdb7d
jvanbiervliet <jan.van.biervliet@etion.be>
2019-03-29 10:22:14 +0100

    oop start

--- uDemy/bootcamp/027_oop_start.py
+++ uDemy/bootcamp/027_oop_start.py
@@ -1,6 +1,17 @@
 class User:
-    pass
+    
+    def __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.__msg = "Massage!"
 
-user1 = User()
-print(user1)
 
+user1 = User('jakke', 37, 74.25)
+user2 = User('pjöttr', 38, 65.32)
+user3 = User('kleeneX', 37, 80.20)
+
+# print(f"{user2.name}, age {user2.age}, weight {user2.weight}")
+
+# print(dir(user1))
+print(user1._User__msg)
