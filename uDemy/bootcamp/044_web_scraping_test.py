0c2d08f7c44b9f78bec29a91ca09a0a4b61b35f7
jvanbiervliet <jan.van.biervliet@etion.be>
2019-05-10 09:09:14 +0200

    web scraping test

--- uDemy/bootcamp/044_web_scraping_test.py
+++ uDemy/bootcamp/044_web_scraping_test.py
@@ -11,18 +11,18 @@ def retrieve_page_content(url):
 
 def seek_elements(content, selector="div"):
     selectable_content = BeautifulSoup(content, "html.parser")
-    return selectable_content.select(selector)
+    return selectable_content.find_all(selector)
 
 
 def write_to_file(content):
     pad = "C:/DEV/Python3/Oefenen/uDemy/bootcamp/docs/scrape.txt"
     with open(pad, mode='w', encoding='utf8', newline='') as scrape_file:
-        html_writer = writer(scrape_file)
-        html_writer.writerows(content)
+        for el in content:
+            scrape_file.write(el.text)
 
 
 if __name__ == "__main__":
-    url = "https://www.rithmschool.com/blog"
+    url = "https://www.etion.be"
     response_content = retrieve_page_content(url)
-    content = seek_elements(response_content)
-    write_to_file(content)
+    divs = seek_elements(response_content, selector="div")    
+    write_to_file(divs)
