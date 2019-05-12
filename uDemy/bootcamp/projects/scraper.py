f341c86639397250e89cefe10d9880ecc247f1a1
Jan Van Biervliet <jvbiervliet@gmail.com>
2019-05-12 19:58:12 +0200

    scraping quotes

--- uDemy/bootcamp/projects/scraper.py
+++ uDemy/bootcamp/projects/scraper.py
@@ -5,9 +5,10 @@ from csv import reader, writer
 
 class Scraper():
     """Scraper class facilitates traversing of html content"""
+
     def __init__(self, url: str):
         """Initialize Scraper. 
-        
+
         path: url/page        
         html: html elements list from content
         """
@@ -15,15 +16,25 @@ class Scraper():
         self.quotes_file_path = "C:/DEV/Python_code/udemy_tutorial/uDemy/bootcamp/docs/quotes.txt"
         self.quotes_list = []
 
-    def funcname(self, parameter_list):
-        pass
-    
-    def build_quotes_file():
-        # fetch all quotes
-        pass
+    def fetch_quotes_list(self):
+        response = requests.get(self.url)
+        html = BeautifulSoup(response.content, "html.parser")
+        self.quotes_list = html.select(".quote")
+        next_tag = html.select(".next")
+        while next_tag:
+            volgende_pagina = next_tag[0].find("a")["href"]            
+            response = requests.get(self.url + volgende_pagina)
+            html = BeautifulSoup(response.content, "html.parser")
+            self.quotes_list.extend(html.select(".quote"))
+            next_tag = html.select(".next")        
 
-if __name__ == "__main__":    
+    def build_quotes_file(self):
+        with open(self.quotes_file_path, mode="w", encoding='utf8', newline='') as quotes_file:
+            quotes_csv = writer(quotes_file)
+            quotes_csv.writerows(self.quotes_list)
+
+if __name__ == "__main__":
     url = "http://quotes.toscrape.com"
     scraper = Scraper(url)
+    scraper.fetch_quotes_list()
     scraper.build_quotes_file()
- 
