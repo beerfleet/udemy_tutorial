8f1b5422fb000043f3cbb092e0efaa70452fb474
Jan Van Biervliet <jvbiervliet@gmail.com>
2019-05-12 18:01:01 +0200

    scraper restart

--- uDemy/bootcamp/projects/scraper.py
+++ uDemy/bootcamp/projects/scraper.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
+from csv import reader, writer
 
 
 class Scraper():
@@ -11,11 +11,19 @@ class Scraper():
         path: url/page        
         html: html elements list from content
         """
-        self.url = url            
+        self.url = url
+        self.quotes_file_path = "C:/DEV/Python_code/udemy_tutorial/uDemy/bootcamp/docs/quotes.txt"
+        self.quotes_list = []
 
+    def funcname(self, parameter_list):
+        pass
+    
     def build_quotes_file():
+        # fetch all quotes
+        pass
 
 if __name__ == "__main__":    
     url = "http://quotes.toscrape.com"
     scraper = Scraper(url)
     scraper.build_quotes_file()
+ 
