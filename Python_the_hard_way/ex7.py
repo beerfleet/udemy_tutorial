157413d341387c1829457ab9e0bf3f3ee2bc43b6
jvanbiervliet <jan.van.biervliet@etion.be>
2019-05-15 15:57:21 +0200

    starting python the hard way

---
+++ Python_the_hard_way/ex7.py
@@ -0,0 +1,11 @@
+# printing
+end1 = "T"
+end2 = "u"
+end3 = "c"
+end4 = "k"
+end5 = "O"
+end6 = "f"
+end7 = "f"
+
+print(end1 + end2 + end3 + end4, end="\n")
+print(end5 + end6 + end7, sep=" wakawaka ")
---
+++ Python_the_hard_way/ex9.py
@@ -0,0 +1,10 @@
+# Printing, printing, printing
+days = "Ma Di Wo Do Vr Za Zo"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun"
+print("The days: ", days)
+print("The months:", months)
+print("""
+Modderfokker waarom
+schrijf je niet ???
+Ik type dit toch uit ????
+""")
