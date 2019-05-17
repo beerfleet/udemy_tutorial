9e2eea2739329f827ebb6a35bcb8aad2008ca316
jvanbiervliet <jan.van.biervliet@etion.be>
2019-05-17 20:38:23 +0200

    even more regex

--- uDemy/bootcamp/045_regex_ex.py
+++ uDemy/bootcamp/045_regex_ex.py
@@ -7,4 +7,20 @@ def is_valid_time(time_str):
         return True
     return False
 
-print(is_valid_time("17:45"))
+def parse_bytes(date_str):
+    bin_regex = re.compile(r'\b[10]{8}\b')    
+    #return bin_regex.findall(date_str)
+    print(bin_regex.findall(date_str))
+
+def parse_date(date_str):
+    date_regex = re.compile(r'(?P<d>\d{2})[/.,](?P<m>\d{2})[/.,](?P<y>\d{4})')
+    match = date_regex.fullmatch(date_str)
+    indexes = ('d', 'm', 'y')
+    groups = match.groups()
+    print(groups)
+    zip_obj = list(zip(groups, indexes))
+    print(zip_obj)
+    return {tup[0]:tup[1] for tup in zip_obj}
+    # return zip_obj
+
+print(parse_date('11/11/1111'))
