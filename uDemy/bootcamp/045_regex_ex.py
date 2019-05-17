d7a1d4c94874278acfd300993a59ebc7965a9425
Jan Van Biervliet <jvbiervliet@gmail.com>
2019-05-17 21:11:16 +0200

    regex ex done

--- uDemy/bootcamp/045_regex_ex.py
+++ uDemy/bootcamp/045_regex_ex.py
@@ -16,11 +16,11 @@ def parse_date(date_str):
     date_regex = re.compile(r'(?P<d>\d{2})[/.,](?P<m>\d{2})[/.,](?P<y>\d{4})')
     match = date_regex.fullmatch(date_str)
     indexes = ('d', 'm', 'y')
-    groups = match.groups()
-    print(groups)
-    zip_obj = list(zip(groups, indexes))
-    print(zip_obj)
-    return {tup[0]:tup[1] for tup in zip_obj}
+    if match:
+        groups = match.groups()    
+        zip_obj = list(zip(groups, indexes))    
+        return {tup[1]:tup[0] for tup in zip_obj}
+    return None
     # return zip_obj
 
-print(parse_date('11/11/1111'))
+print(parse_date('00.10.1111'))
