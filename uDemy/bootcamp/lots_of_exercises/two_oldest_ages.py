c95d3d94e0a74765aeef70aab4d72802bb759652
jvanbiervliet <jan.van.biervliet@etion.be>
2019-05-27 16:37:14 +0200

    two_oldest_ages

---
+++ uDemy/bootcamp/lots_of_exercises/two_oldest_ages.py
@@ -0,0 +1,17 @@
+"""
+. The function should take a list of numbers as its argument and return 
+the two highest numbers within the list . The returned value should be a 
+list in the format [second oldest age, oldest age]
+"""
+
+def two_oldest_ages(lijst: list):
+  lijst.sort()
+  return lijst[-2:]
+
+print(two_oldest_ages( [4,25,3,20,19,5] ))
+
+'''
+two_oldest_ages( [1, 2, 10, 8] ) # [8, 10]
+two_oldest_ages( [6,1,9,10,4] ) # [9,10]
+two_oldest_ages( [4,25,3,20,19,5] ) # [20,25]
+'''
