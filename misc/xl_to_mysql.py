9afe877208287933eb1cd1ebf75d8127d4f64929
jvanbiervliet <jan.van.biervliet@etion.be>
2019-06-05 12:49:00 +0200

    temp

--- misc/xl_to_mysql.py
+++ misc/xl_to_mysql.py
@@ -1,6 +1,7 @@
 import openpyxl
 import datetime
-import mysqlhelper
+import time
+from mysqlhelper import MySqlHelper
 
 query = f"INSERT INTO opslag (datum, beschikbaar) VALUES (%s,%s)"
 xl = 'W:/03_Ondersteuning/03_03_ICT/Prive/Infra/Storage/W-schijf-storage-afname.xlsx'
@@ -8,14 +9,18 @@ xl = 'W:/03_Ondersteuning/03_03_ICT/Prive/Infra/Storage/W-schijf-storage-afname.
 # returns list of tuples
 def acquire_insert_data():
   # laad werkboek
-  doc = openpyxl.load_workbook(xl)    
+  doc = openpyxl.load_workbook(xl, data_only=True)
   # laad sheet
   sheet = doc['Plot']
   # kolom C: datums
   col_c = sheet['C']
   # kolom D: % vrij
   col_d = sheet['D']
-  for cell in col_c:
-    print(cell)
+  return [(datetime.datetime(el[0].value).strftime('%Y-%m-%d'), el[1].value) for el in zip(col_c, col_d)]
 
-acquire_insert_data()
+
+if __name__ == "__main__":
+  insert_data = acquire_insert_data()
+  print(insert_data)
+  # mysql = MySqlHelper('localhost', 'root', '', 'pydb')
+  # mysql.execute_query(query, insert_data)
